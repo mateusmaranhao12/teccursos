7cc315c85ae4c47afa62883fa46af1398da8d37d
mateusmaranhao12 <86203961+mateusmaranhao12@users.noreply.github.com>
2022-02-10 14:31:54 -0300

    Arquivos do projeto

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+let mainNav = document.getElementById('js-menu')
+let navBarToggle = document.getElementById('js-navbar-toggle')
+
+navBarToggle.addEventListener('click', function () {
+    mainNav.classList.toggle('active');
+});
+
+//Links
+
+var curso
+
+function destaque1() {
+    curso = window.open('https://www.udemy.com/course/web-completo/')
+}
+
+function destaque2() {
+    curso = window.open('https://www.udemy.com/course/curso-completo-do-desenvolvedor-laravel/')
+}
+
+function destaque3() {
+    curso = window.open('https://www.udemy.com/course/curso-algoritmos-logica-de-programacao/')
+}
+
+function design1() {
+    curso = window.open('https://www.udemy.com/course/after-effects-photoshop-e-illustrator-para-motion-graphics/')
+}
+
+function design2() {
+    curso = window.open('https://www.udemy.com/course/after_motion/')
+}
+
+function design3() {
+    curso = window.open('https://www.udemy.com/course/after-effects-composicao-motion-graphics-e-vfx/')
+}
+
+function py1() {
+    curso = window.open('https://www.udemy.com/course/python-3-do-zero-ao-avancado/')
+}
+
+function py2() {
+    curso = window.open('https://www.udemy.com/course/curso-de-programacao-em-python-do-basico-ao-avancado/')
+}
+
+function py3() {
+    curso = window.open('https://www.udemy.com/course/programacao-python-do-basico-ao-avancado/')
+}
+
+function py4() {
+    curso = window.open('https://www.udemy.com/course/cientista-de-dados/')
+}
+
+function py5() {
+    curso = window.open('https://www.udemy.com/course/curso-python-3-completo/')
+}
+
+function py6() {
+    curso = window.open('https://www.udemy.com/course/python-do-basico-ao-avancado1500-min-de-exerciciosextras/')
+}
